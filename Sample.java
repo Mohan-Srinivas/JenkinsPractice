5f7344e4fe05e944d2d77f6af95146af37593144
Mohan Srinivas <52268698+Mohan-Srinivas@users.noreply.github.com>
2022-08-18 19:16:22 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,19 +5,6 @@ public class Sample
       System.out.println("Hello Earth");     
       System.out.println("Hello Asgard");
       System.out.println("Hello Xander");
-      System.out.println("Hello Vormir");
-      System.out.println("Hello Nidavelir");
-      System.out.println("Hello Titan");
-    System.out.println("Hello Nova");
-        System.out.println("jisj");
-        System.out.println("jishhdj");
-        
-        System.out.println("jishhdj");
-        System.out.println("Niva");
-        System.out.println("Knowhere");
-        
-        
-        
-        
+      System.out.println("Hello Vormir");      
     }
 }
