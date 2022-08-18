824fe1543eafd05b9887187c99c47f7bc9ddc388
Mohan Srinivas <52268698+Mohan-Srinivas@users.noreply.github.com>
2022-08-18 16:03:13 +0530

    Initial commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample
+{
+    public static void main()
+    {
+      System.out.println("Hello Earth");     
+      System.out.println("Hello Asgard");
+      System.out.println("Hello Xander");
+      System.out.println("Hello Vormir");
+    }
+}
