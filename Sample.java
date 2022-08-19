921d93c3aff5bd5854795b3255d17883fe16bd41
Mohan Srinivas <52268698+Mohan-Srinivas@users.noreply.github.com>
2022-08-19 21:20:10 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -6,14 +6,7 @@ public class Sample
       System.out.println("Hello Asgard");
       System.out.println("Hello Xander");
       System.out.println("Hello Vormir");
-      System.out.println("Hello Nova");
-      System.out.println("Hello Quantum");
-      System.out.println("Hello Ego");
-      System.out.println("Hello Knowhere");
-      System.out.println("Hello Planet X"); 
-      System.out.println("Hello GT");
-        System.out.println("Hello GT1");
-        System.out.println("Hello GT2");
+
       
     }
 }
